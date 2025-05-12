fce2b51f6803cfcbc6d5dba360610148058d2f15
Rizal <rizal2726f@gmail.com>
2025-05-12 12:07:31 +0700

    update

--- lib/history/depan.dart
+++ lib/history/depan.dart
@@ -356,14 +356,21 @@ class _HistoryScreenState extends State<HistoryScreen> {
                             border: Border.all(
                                 color: const Color.fromARGB(255, 101, 19, 116)),
                             borderRadius: BorderRadius.circular(30.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.1),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                                offset: const Offset(0, 3),
+                              ),
+                            ],
                           ),
                           child: InkWell(
                             onTap: () async {
                               final DateTimeRange? picked =
                                   await showDateRangePicker(
                                 context: context,
-                                useRootNavigator:
-                                    false, // <-- ini yang bikin dia gak fullscreen
+                                useRootNavigator: false,
                                 firstDate: DateTime(2000),
                                 lastDate: DateTime.now(),
                                 initialDateRange: DateTimeRange(
@@ -373,15 +380,30 @@ class _HistoryScreenState extends State<HistoryScreen> {
                                 ),
                                 builder: (BuildContext context, Widget? child) {
                                   return Center(
-                                    // biar dia muncul di tengah dengan ukuran kecil
-                                    child: ConstrainedBox(
-                                      constraints:
-                                          const BoxConstraints(maxWidth: 400),
-                                      child: child,
+                                    child: Container(
+                                      margin: const EdgeInsets.all(20),
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(16),
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color:
+                                                Colors.black.withOpacity(0.2),
+                                            spreadRadius: 1,
+                                            blurRadius: 8,
+                                            offset: const Offset(0, 4),
+                                          ),
+                                        ],
+                                      ),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(16),
+                                        child: child,
+                                      ),
                                     ),
                                   );
                                 },
                               );
+
                               if (picked != null) {
                                 setState(() {
                                   filterDataByDateRange(
@@ -389,21 +411,28 @@ class _HistoryScreenState extends State<HistoryScreen> {
                                 });
                               }
                             },
-                            child: const Padding(
+                            child: Padding(
                               padding: EdgeInsets.symmetric(
                                   horizontal: 16.0, vertical: 12.0),
                               child: Row(
                                 children: [
+                                  Icon(
+                                    Icons.date_range,
+                                    color: Color.fromARGB(255, 101, 19, 116),
+                                  ),
+                                  SizedBox(width: 8),
                                   Text(
-                                    'Filter',
+                                    'Select Date Range',
                                     style: TextStyle(
-                                      color: Color.fromARGB(255, 101, 19, 116),
+                                      color: const Color.fromARGB(
+                                          255, 101, 19, 116),
                                       fontWeight: FontWeight.bold,
                                     ),
                                   ),
                                   Icon(
-                                    Icons.arrow_drop_down,
-                                    color: Colors.orange,
+                                    Icons.arrow_forward_ios,
+                                    color: Colors.orange.shade300,
+                                    size: 16,
                                   ),
                                 ],
                               ),
